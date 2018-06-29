dd1f69b32e164babe8e4f1d157778e362b89fc7d
pc <xth4065@gmail.com>
2018-06-29 10:25:33 +0800

    1st2

---
+++ Client/conf/settings.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/11 9:34
+# @Author  : Tianhao
+# @Email   : xth9363@163.com
+# @File    : settings.py
+# @Software: PyCharm
+
+
+# -*- coding:utf-8 -*-
+
+import os
+
+# 远端服务器配置
+Params = {
+    "server": "127.0.0.1",
+    "port": 8000,
+    'url': '/assets/report/',
+    'request_timeout': 30,
+}
+
+# 日志文件配置
+
+PATH = os.path.join(os.path.dirname(os.getcwd()), 'log', 'cmdb.log')
